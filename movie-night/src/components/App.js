dbe2d2fecfad0aee74daad4ac55044ce5470d51e
Melissa Sedgwick <melissa.sedgwick@yahoo.co.uk>
2019-03-29 10:24:07 +0000

    Initial commit - set up react app project

---
+++ movie-night/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <h1>Movie Night</h1>
+      </div>
+    );
+  }
+}
+
+export default App
