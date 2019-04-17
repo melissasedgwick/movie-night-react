716f2cfa0df8dc9f09a0a6e7ee158dc5e93e7604
Melissa Sedgwick <melissa.sedgwick@yahoo.co.uk>
2019-04-17 11:51:27 +0100

    Added redux-form and created form for adding new movie list

--- movie-night/src/pages/ListCreate/index.js
+++ movie-night/src/pages/ListCreate/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-class ListCreate extends React.Component {
+export class ListCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    } else { return null };
+  };
+
+  renderInput = ({ input, label, meta }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
       <div>
-        <h2>ListCreate</h2>
+        <h2 className="ui centre aligned icon header">Create a new movie list!</h2>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+          <Field name="title" component={this.renderInput} label="Title:"/>
+          <button className="ui button primary">Submit</button>
+        </form>
       </div>
     );
   };
 };
 
-export default ListCreate;
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) { errors.title = 'Please enter a title for your movie list' };
+
+  return errors;
+};
+
+
+export default reduxForm({
+  form: 'listCreate',
+  validate
+})(ListCreate);
--- movie-night/src/pages/ListCreate/spec.js
+++ movie-night/src/pages/ListCreate/spec.js
@@ -1,11 +1,140 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import ListCreate from './';
+import { shallow } from 'enzyme';
+import { ListCreate } from './';
+
+const props = { handleSubmit: jest.fn() };
 
 describe('<ListCreate />', () => {
   it('should render', () => {
-    const wrapper = mount(<ListCreate />);
+    const wrapper = shallow(<ListCreate {...props} />);
 
     expect(wrapper.length).toEqual(1);
+  });
+
+  describe('renderError', () => {
+    it('should be a function', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderError } = wrapper.instance();
+
+      expect(renderError).toBeInstanceOf(Function);
+    });
+
+    it('should return an error message when touched and error are true', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderError } = wrapper.instance();
+
+      const result = renderError({ error: 'test error', touched: true }).props.children
+
+      expect(result.type).toEqual('div');
+      expect(result.props.children).toEqual('test error');
+    });
+
+    it('should return null when touched is false', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderError } = wrapper.instance();
+
+      const result = renderError({ error: 'test error', touched: false });
+
+      expect(result).toEqual(null);
+    });
+
+    it('should return null when there is no error', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderError } = wrapper.instance();
+
+      const result = renderError({ touched: true });
+
+      expect(result).toEqual(null);
+    });
+  });
+
+  describe('renderInput', () => {
+    const input = { name: 'title', value: 'text' };
+    const label = 'title';
+    const meta = { touched: true };
+
+    it('should be a function', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderInput } = wrapper.instance();
+
+      expect(renderInput).toBeInstanceOf(Function);
+    });
+
+    it('should return a label with the correct text', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderInput } = wrapper.instance();
+
+      const result = renderInput({input, label, meta}).props.children;
+
+      expect(result[0].type).toEqual('label');
+      expect(result[0].props.children).toEqual('title');
+    });
+
+    it('should return an input with the correct name and value', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderInput } = wrapper.instance();
+
+      const result = renderInput({input, label, meta}).props.children;
+
+      expect(result[1].type).toEqual('input');
+      expect(result[1].props.name).toEqual('title');
+      expect(result[1].props.value).toEqual('text');
+    });
+
+    it('should call renderError', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { renderInput } = wrapper.instance();
+      const renderError = jest.spyOn(wrapper.instance(), 'renderError');
+
+      renderInput({input, label, meta});
+
+      expect(renderError).toHaveBeenCalledWith(meta);
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('should be a function', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const { onSubmit } = wrapper.instance();
+
+      expect(onSubmit).toBeInstanceOf(Function);
+    });
+  });
+
+  describe('header', () => {
+    it('should be rendered within ListCreate with the correct text', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const header = wrapper.find('h2');
+
+      expect(header.length).toEqual(1);
+      expect(header.text()).toEqual('Create a new movie list!');
+    });
+  });
+
+  describe('form', () => {
+    it('should be rendered within ListCreate', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const form = wrapper.find('form');
+
+      expect(form.length).toEqual(1);
+    });
+
+    it('should render one Field with the correct label', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const form = wrapper.find('form');
+      const field = form.find('Field');
+
+      expect(field.length).toEqual(1);
+      expect(field.props().label).toEqual('Title:');
+    });
+
+    it('should render one button with the correct text', () => {
+      const wrapper = shallow(<ListCreate {...props} />);
+      const form = wrapper.find('form');
+      const button = form.find('button');
+
+      expect(button.length).toEqual(1);
+      expect(button.text()).toEqual('Submit');
+    });
   });
 });
