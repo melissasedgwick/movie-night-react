7715320eed8bb1e84968ae67881202244092040c
Melissa Sedgwick <melissa.sedgwick@yahoo.co.uk>
2019-05-02 09:31:21 +0100

    Refactored ListEdit page to use ListForm component

--- movie-night/src/pages/ListEdit/index.js
+++ movie-night/src/pages/ListEdit/index.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { fetchList, editList } from '../../actions';
+import ListForm from '../../components/ListForm/';
 
 export class ListEdit extends React.Component {
   componentDidMount() {
     this.props.fetchList(this.props.match.params.id);
-  }
-
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    } else { return null };
-  };
-
-  renderInput = ({ input, label, meta }) => {
-    return (
-      <div className="field">
-        <label>{label}</label>
-        <input {...input} />
-        {this.renderError(meta)}
-      </div>
-    );
   }
 
   onSubmit = (formValues) => {
@@ -36,10 +16,7 @@ export class ListEdit extends React.Component {
     return (
       <div>
         <h2 className="ui centre aligned icon header">Edit List</h2>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-          <Field name="title" component={this.renderInput} label="Title:" />
-          <button className="ui button primary">Submit Changes</button>
-        </form>
+        <ListForm onSubmit={this.onSubmit} />
       </div>
     );
   };
@@ -49,17 +26,4 @@ const mapStateToProps = (state, ownProps) => {
   return { lists: state.list[ownProps.match.params.id] };
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) { errors.title = 'Please enter a title for your movie list' };
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'listEdit',
-  validate
-})(ListEdit);
-
-export default connect(mapStateToProps, { fetchList, editList })(formWrapped);
+export default connect(mapStateToProps, { fetchList, editList })(ListEdit);
--- movie-night/src/pages/ListEdit/spec.js
+++ movie-night/src/pages/ListEdit/spec.js
@@ -3,11 +3,9 @@ import { shallow } from 'enzyme';
 import { ListEdit } from './';
 
 describe('<ListEdit />', () => {
-  const handleSubmit = jest.fn();
   const fetchList = jest.fn();
 
   const props = {
-    handleSubmit,
     fetchList,
     match: { params: { id: 1 } }
   }
@@ -24,73 +22,6 @@ describe('<ListEdit />', () => {
       wrapper.instance().componentDidMount();
 
       expect(fetchList).toHaveBeenCalledWith(1);
-    });
-  });
-
-  describe('renderError', () => {
-    it('should return an error message when touched and error are true', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderError } = wrapper.instance();
-
-      const result = renderError({ error: 'test error', touched: true }).props.children
-
-      expect(result.type).toEqual('div');
-      expect(result.props.children).toEqual('test error');
-    });
-
-    it('should return null when touched is false', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderError } = wrapper.instance();
-
-      const result = renderError({ error: 'test error', touched: false });
-
-      expect(result).toEqual(null);
-    });
-
-    it('should return null when there is no error', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderError } = wrapper.instance();
-
-      const result = renderError({ touched: true });
-
-      expect(result).toEqual(null);
-    });
-  });
-
-  describe('renderInput', () => {
-    const input = { name: 'title', value: 'text' };
-    const label = 'title';
-    const meta = { touched: true };
-
-    it('should return a label with the correct text', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderInput } = wrapper.instance();
-
-      const result = renderInput({input, label, meta}).props.children;
-
-      expect(result[0].type).toEqual('label');
-      expect(result[0].props.children).toEqual('title');
-    });
-
-    it('should return an input with the correct name and value', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderInput } = wrapper.instance();
-
-      const result = renderInput({input, label, meta}).props.children;
-
-      expect(result[1].type).toEqual('input');
-      expect(result[1].props.name).toEqual('title');
-      expect(result[1].props.value).toEqual('text');
-    });
-
-    it('should call renderError', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const { renderInput } = wrapper.instance();
-      const renderError = jest.spyOn(wrapper.instance(), 'renderError');
-
-      renderInput({input, label, meta});
-
-      expect(renderError).toHaveBeenCalledWith(meta);
     });
   });
 
@@ -115,29 +46,12 @@ describe('<ListEdit />', () => {
       expect(header.text()).toEqual('Edit List');
     });
 
-    it('should render a form', () => {
+    it('should render a ReduxForm with an onSubmit prop', () => {
       const wrapper = shallow(<ListEdit {...props} />);
-      const form = wrapper.find('form');
+      const reduxForm = wrapper.find('ReduxForm');
 
-      expect(form.length).toEqual(1);
-    });
-
-    it('form should render one Field with the correct label', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const form = wrapper.find('form');
-      const field = form.find('Field');
-
-      expect(field.length).toEqual(1);
-      expect(field.props().label).toEqual('Title:');
-    });
-
-    it('form should render one button with the correct text', () => {
-      const wrapper = shallow(<ListEdit {...props} />);
-      const form = wrapper.find('form');
-      const button = form.find('button');
-
-      expect(button.length).toEqual(1);
-      expect(button.text()).toEqual('Submit Changes');
+      expect(reduxForm.length).toEqual(1);
+      expect(reduxForm.props().onSubmit).toBeInstanceOf(Function);
     });
   });
 });
